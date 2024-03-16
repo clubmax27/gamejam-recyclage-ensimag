ca8b710821c06018028af8446752ff5a7d3e6bb9
pakinj <Jacques.Pakin@grenoble-inp.org>
2024-03-16 17:00:24 +0100

    bug tour que sur les boutons

--- Scenes/GameScene.gd
+++ Scenes/GameScene.gd
@@ -11,7 +11,8 @@ var build_type
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("ready game scene avant node")	
-	map_node = get_node("Level_1")  ## sélectionner la carte
+	map_node = get_node("Level")  ## sélectionner la carte
+	print(map_node)
 	print("ready game scene")
 	print(get_tree().get_nodes_in_group("build_buttons"))
 	for button in get_tree().get_nodes_in_group("build_buttons"):  ## pour que cela fonctionne pour toutes les tours et récupère le nom
@@ -26,22 +27,25 @@ func _process(delta):
 
 func _unhandled_input(event):
 	if event.is_action_released("ui_cancel") and build_mode:
+		print(build_mode, "unhandle cancel")
 		cancel_build_mode()
 	if event.is_action_released("ui_accept") and (not build_mode):
+		print(build_mode, "unhandle build")		
 		verify_and_build()
 		cancel_build_mode()
 	
 func  initiate_build_mode(tower_type):
 	build_type = tower_type + "T1"
 	build_mode = true
+	print("dans build mode")
 	get_node("UI").set_tower_preview(build_type, get_global_mouse_position())
 
 func update_tower_preview():
 	var mouse_position = get_global_mouse_position()
-	var current_tile = map_node.get_node("TowerExclusion").world_to_map(mouse_position)
-	var tile_position =  map_node.get_node("TowerExclusion").map_to_world(current_tile)
+	var current_tile = map_node.get_node("TowerExclusion").local_to_map(mouse_position)
+	var tile_position =  map_node.get_node("TowerExclusion").map_to_local(current_tile)
 	
-	if map_node.get_node("TowerExclusion").get_cellv(current_tile) == -1:
+	if map_node.get_node("TowerExclusion").get_cell_source_id(0, current_tile) == -1:
 		get_node("UI").update_tower_preview(tile_position, "ad54ff3c")
 		build_valid = true
 		build_location = tile_position
@@ -58,7 +62,7 @@ func verify_and_build():
 	if build_valid:
 		## on peut verifier les conditions de ressources ici
 	
-		var new_tower = load("Turret_Bow_1.tscn").instance()  ## modifier avec le chemin d'acces de la tour
+		var new_tower = load("res://Scenes/Turret_Bow_1.tscn").instance()  ## modifier avec le chemin d'acces de la tour
 		new_tower.position = build_location
 		map_node.get_node("Towers").add_child(new_tower, true)
 		
--- Scenes/UI.gd
+++ Scenes/UI.gd
@@ -1,19 +1,18 @@
 extends CanvasLayer
 
-
 func set_tower_preview(tower_type, mouse_position):
-	var drag_tower = load("Turret_Bow_1.tscn").instance()  ## a modifier pour mettre le chemin
+	var drag_tower = load("res://Scenes/Turret_Bow_1.tscn").instantiate()  ## a modifier pour mettre le chemin
 	drag_tower.set_name("DragTower")
 	drag_tower.modulate = Color("ad54ff3c")
 	
 	var control = Control.new()
 	control.add_child(drag_tower, true)
-	control.rect_position = mouse_position
+	control.position = mouse_position
 	control.set_name("TowerPreview")
 	add_child(control, true)
 	move_child(get_node("TowerPreview"), 0)
 	
 func update_tower_preview(new_position, color):
-	get_node("TowerPreview").rect_position = new_position
+	get_node("TowerPreview").position = new_position
 	if get_node("TowerPreview/DragTower").modulate != Color(color):
 		get_node("TowerPreview/DragTower").modulate = Color(color)
