51c41550d87733649491e43dd41cee6a215d3a97
Maxence Mathieu <Sans.Plague@gmail.com>
2024-03-17 16:40:49 +0100

    message skip when left click

--- Scenes/Dialogue.gd
+++ Scenes/Dialogue.gd
@@ -1,15 +1,15 @@
 extends Control
 
 var messages = [{
-		"message" : "Human, I remember you're ...... neutral",
+		"message" : "C'est une catastrophe ! A force de se faire vanner sur le fait qu'ils ont rien gagné aux OL'INP'iades, les Phelmas sont en train de se venger en attaquant l'ENSIMAG !",
 		"character" : 0,
 	},
 	{
-		"message" : "My Second Message",
+		"message" : "J'adore le JAVA",
 		"character" : 1,
 	},
 	{
-		"message" : "My Third Message",
+		"message" : "ah oeeeeeeeeeeeeeeeeeee",
 		"character" : 0,
 	}
 ]
@@ -20,6 +20,8 @@ var read_time = 2
 var current_message = 0
 var display = ""
 var current_char = 0
+
+var message_still_writing = true
 
 func _ready():
 	start_dialogue()
@@ -41,22 +43,32 @@ func start_dialogue():
 		$Dialogue/TextBackground/Maxence.set_visible(false)
 
 func stop_dialogue():
-	# get_parent().remove_child(self)
 	set_visible(false)
 
 func _input(ev):
 	if Input.is_key_pressed(KEY_ENTER) or (Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)):
-		next_message()
+		if message_still_writing:
+			finish_message()
+		else:
+			next_message()
 		
 func _on_next_char_timer_timeout():
 	if (current_char < len(messages[current_message]["message"])):
+		message_still_writing = true
+		
 		var next_char = messages[current_message]["message"][current_char]
 		display += next_char
 		
 		$Dialogue/TextBackground/MarginContainer/Message.text = display
 		current_char += 1
 	else:
+		message_still_writing = false
 		$Dialogue/TextBackground/MarginContainer/NextCharTimer.stop()
+
+func finish_message():
+	message_still_writing = false
+	$Dialogue/TextBackground/MarginContainer/NextCharTimer.stop()
+	$Dialogue/TextBackground/MarginContainer/Message.text = messages[current_message]["message"]
 
 func next_message():
 	if (current_message == len(messages) - 1):
