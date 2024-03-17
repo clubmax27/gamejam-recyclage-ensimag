66e0972ac82fcd98cfed77b689e3971693906bc5
Maxence Mathieu <Sans.Plague@gmail.com>
2024-03-17 12:07:01 +0100

    overlay added

--- Scenes/UI.gd
+++ Scenes/UI.gd
@@ -7,8 +7,18 @@ func set_tower_preview(tower_type, mouse_position):
 	drag_tower.set_name("DragTower")
 	drag_tower.modulate = Color("ad54ff3c")
 	
+	var range_texture = Sprite2D.new()
+	range_texture.set_name("RangeIndicator")
+	range_texture.position = Vector2(0, 0)
+	var scaling = GameData.tower_data[tower_type + "_1"].range / 500.0
+	range_texture.scale = Vector2(scaling, scaling)
+	var texture = load("res://Assets/range_overlay.png")
+	range_texture.texture = texture
+	range_texture.modulate = Color("ad54ff3c")
+	
 	var control = Control.new()
 	control.add_child(drag_tower, true)
+	control.add_child(range_texture)
 	control.position = mouse_position
 	control.set_name("TowerPreview")
 	add_child(control, true)
@@ -18,6 +28,7 @@ func update_tower_preview(new_position, color):
 	get_node("TowerPreview").position = new_position
 	if get_node("TowerPreview/DragTower").modulate != Color(color):
 		get_node("TowerPreview/DragTower").modulate = Color(color)
+		get_node("TowerPreview/RangeIndicator").modulate = Color(color)
 
 
 # GAME CONTROL
